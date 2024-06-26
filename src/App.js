46f49da459808de48af7242024f80d10bb16c99d
Poyonov O'rolbek <151833182+Uralbek-2004@users.noreply.github.com>
2024-06-26 16:34:30 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <ul>
+        <li>
+          <img src="" alt="" />
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">settings</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
